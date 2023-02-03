338a137b7fba7b99eeb4b29d7603d27460ab696d
otash8110 <otash_baxadirov@mail.ru>
2023-02-03 14:08:45 +0500

    update basic table component

--- src/pages/tables.vue
+++ src/pages/tables.vue
@@ -4,10 +4,43 @@ import TableDark from '@/views/user-interface/tables/TableDark.vue'
 import TableDensity from '@/views/user-interface/tables/TableDensity.vue'
 import TableHeight from '@/views/user-interface/tables/TableHeight.vue'
 import TableFixedHeader from '@/views/user-interface/tables/TableFixedHeader.vue'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import Admin from "../services/adminServices/AdminService"
 
 const data = ref()
+const array = ref({
+  heads: [
+    {
+      value: "Dessert (100g serving)",
+    },
+    {
+      value: "Calories",
+    },
+    {
+      value: "Fat (g)",
+    },
+    {
+      value: "Carbs (g)",
+    },
+    {
+      value: "Protein (g)",
+    },
+  ],
+  data: [{
+    dessert: 'Frozen Yogurt',
+    calories: 159,
+    fat: 6,
+    carbs: 24,
+    protein: 4,
+  },
+  {
+    dessert: 'Ice cream sandwich',
+    calories: 237,
+    fat: 6,
+    carbs: 24,
+    protein: 4,
+  }],
+})
 
 onMounted(() => {
   Admin.getUnregisteredUsers().then(result => {
@@ -21,7 +54,7 @@ onMounted(() => {
     <!-- basic -->
     <VCol cols="12">
       <VCard title="Basic">
-        <TableBasic />
+        <TableBasic :elements="array" />
       </VCard>
     </VCol>
   </VRow>
--- src/views/user-interface/tables/TableBasic.vue
+++ src/views/user-interface/tables/TableBasic.vue
@@ -1,67 +1,28 @@
 <script setup>
-const desserts = [
-  {
-    dessert: 'Frozen Yogurt',
-    calories: 159,
-    fat: 6,
-    carbs: 24,
-    protein: 4,
+const props = defineProps({
+  elements: {
+    type: Array,
+    default: null,
   },
-  {
-    dessert: 'Ice cream sandwich',
-    calories: 237,
-    fat: 6,
-    carbs: 24,
-    protein: 4,
-  },
-  {
-    dessert: 'Eclair',
-    calories: 262,
-    fat: 6,
-    carbs: 24,
-    protein: 4,
-  },
-  {
-    dessert: 'Cupcake',
-    calories: 305,
-    fat: 6,
-    carbs: 24,
-    protein: 4,
-  },
-  {
-    dessert: 'Gingerbread',
-    calories: 356,
-    fat: 6,
-    carbs: 24,
-    protein: 4,
-  },
-]
+})
 </script>
 
 <template>
   <VTable>
     <thead>
       <tr>
-        <th class="text-uppercase">
-          Dessert (100g serving)
-        </th>
-        <th class="text-center text-uppercase">
-          Calories
-        </th>
-        <th class="text-center text-uppercase">
-          Fat (g)
-        </th>
-        <th class="text-center text-uppercase">
-          Carbs (g)
-        </th>
-        <th class="text-center text-uppercase">
-          Protein (g)
+        <th
+          v-for="item in elements.heads"
+          :key="item.value"
+          class="text-center"
+        >
+          {{ item.value }}
         </th>
       </tr>
     </thead>
     <tbody>
       <tr
-        v-for="item in desserts"
+        v-for="item in elements.data"
         :key="item.dessert"
       >
         <td>{{ item.dessert }}</td>
